4b52db2a412d0c095151bbf083a7990d01f59b20
mcalmette <mcalmette@gmail.com>
2020-05-30 23:53:29 -0700

    Commit

--- MultPlot.py
+++ MultPlot.py
@@ -1,8 +1,35 @@
+
+import os
 import pandas as pd
+import matplotlib.pyplot as plt
+
+def symbol_to_path(symbol, base_dir="data"):
+    """Return CSV file path given ticker symbol."""
+    return  os.path.join(base_dir, "{}.csv".format(str(symbol)))
+
+def get_data(symbols, dates):
+    """Read Stock data (adjusted close) for given symb"""
+    df = pd.DataFrame(index=dates)
+    if 'SPY' not in symbols: # add SPY for ref if absent
+        symbols.insert(0, 'SPY')
+
+    for symbol in symbols:
+        df_temp = pd.read_csv(symbol_to_path(symbol), index_col='Date',
+                              parse_dates=True, usecols=['Date','Adj Close'], na_values=['nan'])
+        df_temp = df_temp.rename(columns={'Adj Close' : symbol})
+        df = df.join(df_temp)
+        if symbol == 'SPY': #drop dates SPY didnt trade
+            df = df.dropna(subset=[["SPY"]])
+
+    return df
+
+def plot_data(df):
+    df.plot()
+    plt.show()
 
 def run():
-    start_date = '2020-01-22'
-    end_date = '2020-01-24'
+    start_date = '2019-06-22'
+    end_date = '2020-03-24'
     dates = pd.date_range(start_date, end_date)
 
     #create an empty df
@@ -31,7 +58,8 @@ def run():
         df_temp = df_temp.rename(columns={'Adj Close' : symbol})
         df1 = df1.join(df_temp)
 
-    print(df1)
+    plot_data(df1)
+
 
 if __name__ == "__main__":
     run()
